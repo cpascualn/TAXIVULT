7766785eafa1159383a9f28b880ff9188038e053
Carlos <122229211+cpascualn@users.noreply.github.com>
2024-04-23 18:03:16 +0200

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,396 @@
+<script setup>
+
+</script>
+<template>
+    <div class="div">
+        <div class="div-2">
+            <div class="div-3">
+                <div class="div-4">
+                    <div class="column">
+                        <img loading="lazy" srcSet="/img/logo.jpg" class="img" />
+                    </div>
+                    <div class="column-2">
+                        <div class="div-5">
+                            <div class="div-6">
+                                <div class="column-3">
+                                    <div class="div-7">
+                                        <div class="div-8">Empresa</div>
+                                        <div class="div-9">Sobre Nosotros</div>
+                                        <div class="div-10">Soluciones</div>
+                                        <div class="div-11">Insights</div>
+                                    </div>
+                                </div>
+                                <div class="column-4">
+                                    <div class="div-12">
+                                        <div class="div-13">Categorías</div>
+                                        <div class="div-14">Contratar Talento</div>
+                                        <div class="div-15">Desarrollar Talento</div>
+                                        <div class="div-16">Herramientas de Gamificación</div>
+                                    </div>
+                                </div>
+                                <div class="column-5">
+                                    <div class="div-17">
+                                        <div class="div-18">Competencias Digitales</div>
+                                        <div class="div-19">Competencias Comerciales</div>
+                                        <div class="div-20">Liderazgo</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="div-21"></div>
+            <div class="div-22">
+                <img loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/dc47f7097b8d90af98224282559e9b24cff7e34859c27111968dbe50bd3f030f?"
+                    class="img-2" />
+                <div class="div-23">
+                    <div class="div-24">Política de Privacidad</div>
+                    <div class="div-25">Términos y Condiciones</div>
+                    <div class="div-26">Código de Conducta</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.div {
+    background-color: var(--oscuro1, #0a151b);
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 55px 60px;
+}
+
+@media (max-width: 991px) {
+    .div {
+        max-width: 100%;
+        padding: 0 20px;
+    }
+}
+
+.div-2 {
+    display: flex;
+    width: 1076px;
+    max-width: 100%;
+    flex-direction: column;
+}
+
+
+@media (max-width: 991px) {
+    .div-3 {
+        max-width: 100%;
+        padding-right: 20px;
+    }
+}
+
+.div-4 {
+    gap: 20px;
+    display: flex;
+}
+
+@media (max-width: 991px) {
+    .div-4 {
+        flex-direction: column;
+        align-items: stretch;
+        gap: 0px;
+    }
+}
+
+.column {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 29%;
+    margin-left: 0px;
+}
+
+@media (max-width: 991px) {
+    .column {
+        width: 100%;
+    }
+}
+
+.img {
+    aspect-ratio: 1.08;
+    object-fit: auto;
+    object-position: center;
+    width: 136px;
+    max-width: 100%;
+}
+
+@media (max-width: 991px) {
+    .img {
+        margin-top: 40px;
+    }
+}
+
+.column-2 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 71%;
+    margin-left: 20px;
+}
+
+@media (max-width: 991px) {
+    .column-2 {
+        width: 100%;
+    }
+}
+
+.div-5 {
+    flex-grow: 1;
+}
+
+@media (max-width: 991px) {
+    .div-5 {
+        max-width: 100%;
+        margin-top: 40px;
+    }
+}
+
+.div-6 {
+    gap: 20px;
+    display: flex;
+}
+
+@media (max-width: 991px) {
+    .div-6 {
+        flex-direction: column;
+        align-items: stretch;
+        gap: 0px;
+    }
+}
+
+.column-3 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 27%;
+    margin-left: 0px;
+}
+
+@media (max-width: 991px) {
+    .column-3 {
+        width: 100%;
+    }
+}
+
+.div-7 {
+    display: flex;
+    margin-top: 12px;
+    flex-grow: 1;
+    flex-direction: column;
+    font-size: 14px;
+    color: var(--White, var(--blanco, #fff));
+    font-weight: 400;
+    line-height: 150%;
+}
+
+@media (max-width: 991px) {
+    .div-7 {
+        margin-top: 40px;
+    }
+}
+
+.div-8 {
+    font: 16px K2D, sans-serif;
+}
+
+.div-9 {
+    font-family: K2D, sans-serif;
+    margin-top: 49px;
+}
+
+@media (max-width: 991px) {
+    .div-9 {
+        margin-top: 40px;
+    }
+}
+
+.div-10 {
+    font-family: K2D, sans-serif;
+    margin-top: 51px;
+}
+
+@media (max-width: 991px) {
+    .div-10 {
+        margin-top: 40px;
+    }
+}
+
+.div-11 {
+    font-family: K2D, sans-serif;
+    margin-top: 51px;
+}
+
+@media (max-width: 991px) {
+    .div-11 {
+        margin-top: 40px;
+    }
+}
+
+.column-4 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 39%;
+    margin-left: 20px;
+}
+
+@media (max-width: 991px) {
+    .column-4 {
+        width: 100%;
+    }
+}
+
+.div-12 {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+    font-size: 14px;
+    color: var(--White, var(--blanco, #fff));
+    font-weight: 400;
+    line-height: 150%;
+}
+
+@media (max-width: 991px) {
+    .div-12 {
+        margin-top: 40px;
+    }
+}
+
+.div-13 {
+    font: 16px K2D, sans-serif;
+}
+
+.div-14 {
+    font-family: K2D, sans-serif;
+    margin-top: 40px;
+}
+
+.div-15 {
+    font-family: K2D, sans-serif;
+    margin-top: 40px;
+}
+
+.div-16 {
+    font-family: K2D, sans-serif;
+    margin-top: 40px;
+}
+
+.column-5 {
+    display: flex;
+    flex-direction: column;
+    line-height: normal;
+    width: 34%;
+    margin-left: 20px;
+}
+
+@media (max-width: 991px) {
+    .column-5 {
+        width: 100%;
+    }
+}
+
+.div-17 {
+    display: flex;
+    margin-top: 63px;
+    flex-direction: column;
+    font-size: 14px;
+    color: var(--White, var(--blanco, #fff));
+    font-weight: 400;
+    line-height: 150%;
+}
+
+@media (max-width: 991px) {
+    .div-17 {
+        margin-top: 40px;
+    }
+}
+
+.div-18 {
+    font-family: K2D, sans-serif;
+}
+
+.div-19 {
+    font-family: K2D, sans-serif;
+    margin-top: 40px;
+}
+
+.div-20 {
+    font-family: K2D, sans-serif;
+    margin-top: 40px;
+}
+
+.div-21 {
+    background-color: var(--blanco, #fff);
+    margin-top: 72px;
+    height: 1px;
+}
+
+@media (max-width: 991px) {
+    .div-21 {
+        max-width: 100%;
+        margin-top: 40px;
+    }
+}
+
+.div-22 {
+    display: flex;
+    margin-top: 20px;
+    width: 100%;
+    gap: 20px;
+    font-size: 14px;
+    color: var(--White, var(--blanco, #fff));
+    font-weight: 400;
+    line-height: 150%;
+    justify-content: space-between;
+}
+
+@media (max-width: 991px) {
+    .div-22 {
+        max-width: 100%;
+        flex-wrap: wrap;
+    }
+}
+
+.img-2 {
+    aspect-ratio: 5;
+    object-fit: auto;
+    object-position: center;
+    width: 100px;
+    align-self: start;
+    max-width: 100%;
+}
+
+.div-23 {
+    display: flex;
+    gap: 20px;
+    justify-content: space-between;
+}
+
+@media (max-width: 991px) {
+    .div-23 {
+        flex-wrap: wrap;
+    }
+}
+
+.div-24 {
+    font-family: K2D, sans-serif;
+}
+
+.div-25 {
+    font-family: K2D, sans-serif;
+}
+
+.div-26 {
+    font-family: K2D, sans-serif;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Header from "../components/Header.vue";
+import Hero from "../components/Hero.vue";
+import Footer from "../components/Footer.vue";
+import Section1 from "../components/Section1.vue";
+import Section2 from "../components/Section2.vue";
+import Section3 from "../components/Section3.vue";
+</script>
+
+<template>
+  <!-- <Header></Header> -->
+  <Hero></Hero>
+  <main>
+    <Section1></Section1>
+    <Section2></Section2>
+    <Section3></Section3>
+  </main>
+  <Footer></Footer>
+</template>
+
+
+<style scoped></style>
